893dca3f781f789beaad039f15c5304c5c7f7b58
Yashwanth <yashukrish6413@gmail.com>
2025-12-27 23:16:34 +0530

    some changes

--- script.js
+++ script.js
@@ -1,94 +1,135 @@
+/*==================== MENU SHOW Y HIDDEN ====================*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
 
-function myMenuFunction(){
-    var menuBtn = document.getElementById("myNavMenu");
-    if(menuBtn.className === "nav-menu"){
-      menuBtn.className += " responsive";
-    } else {
-      menuBtn.className = "nav-menu";
-    }
-  }
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
 
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
 
-  function welcome() {
-      window.open("https://www.youtube.com/")
-  }
-/* ----- shadow on navigation----- */
-  window.onscroll = function() {headerShadow()};
-  function headerShadow() {
-    const navHeader =document.getElementById("header");
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop >  50) {
-      navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
-      navHeader.style.height = "70px";
-      navHeader.style.lineHeight = "70px";
-    } else {
-      navHeader.style.boxShadow = "none";
-      navHeader.style.height = "90px";
-      navHeader.style.lineHeight = "90px";
-    }
-  }
-/* ----- typing effect ----- */
- var typingEffect = new Typed(".typedText",{
-    strings : ["Yashwanth K","Designer,","Youtuber,","Developer,","Gamer."],
-    loop : true,
-    typeSpeed : 100, 
-    backSpeed : 80,
-    backDelay : 2000
- })
-/* ----- scroll real animation# ----- */
- const sr = ScrollReveal({
-        origin: 'top',
-        distance: '80px',
-        duration: 2000,
-        reset: true     
- })
-/* -- home -- */
-sr.reveal('.featured-text-card',{})
-sr.reveal('.featured-name',{delay: 100})
-sr.reveal('.featured-text-info',{delay: 200})
-sr.reveal('.featured-text-btn',{delay: 200})
-sr.reveal('.social_icons',{delay: 200})
-sr.reveal('.featured-image',{delay: 300})
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
 
-/* -- project boxxxx -- */
-sr.reveal('.project-box',{interval: 200})
-/* -- HEADINGS -- */
-sr.reveal('.top-header',{})
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
-/* -- left right animation and about section-- */
-const srLeft = ScrollReveal({
-  origin: 'left',
-  distance: '80px',
-  duration: 2000,
-  reset: true
-})
-
-srLeft.reveal('.about-info',{delay: 100})
-srLeft.reveal('.contact-info',{delay: 100})
-/* -- form box -- */
-const srRight = ScrollReveal({
-  origin: 'right',
-  distance: '80px',
-  duration: 2000,
-  reset: true
-})
-
-srRight.reveal('.skills-box',{delay: 100})
-srRight.reveal('.form-control',{delay: 100})
-
-/* ----- active linkK ----- */
-
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll('section[id]')
+
 function scrollActive() {
-  const scrollY = window.scrollY;
-  sections.forEach(current =>{
-    const sectionHeight = current.offsetHeight,
-        sectionTop = current.offsetTop - 50,
-      sectionId = current.getAttribute('id')
-    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) { 
-        document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
-    }  else {
-      document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
-    }
-  })
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
 }
 window.addEventListener('scroll', scrollActive)
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+function scrollHeader() {
+    const nav = document.getElementById('header')
+    // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
+    if (this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*==================== TYPING EFFECT ====================*/
+if (document.querySelector('.typed-text')) {
+    var typingEffect = new Typed(".typed-text", {
+        strings: ["Yashwanth K", "Designer,", "Youtuber,", "Developer,", "Gamer."],
+        loop: true,
+        typeSpeed: 100,
+        backSpeed: 80,
+        backDelay: 2000
+    })
+}
+
+/*==================== SCROLL REVEAL ANIMATION ====================*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+/* -- HOME -- */
+sr.reveal('.home__title', {})
+sr.reveal('.home__scroll', { delay: 200 })
+sr.reveal('.home__img', { origin: 'right', delay: 400 })
+
+/* -- ABOUT -- */
+sr.reveal('.about__img', { delay: 500 })
+sr.reveal('.about__subtitle', { delay: 300 })
+sr.reveal('.about__profession', { delay: 400 })
+sr.reveal('.about__text', { delay: 500 })
+sr.reveal('.about__social-icon', { delay: 600, interval: 200 })
+
+/* -- SKILLS -- */
+sr.reveal('.skills__subtitle', {})
+sr.reveal('.skills__name', { distance: '20px', delay: 50, interval: 100 })
+sr.reveal('.skills__img', { delay: 400 })
+
+/* -- PORTFOLIO -- */
+sr.reveal('.project__card', { interval: 200 })
+
+/* -- CONTACT -- */
+sr.reveal('.contact__subtitle', {})
+sr.reveal('.contact__text', { interval: 200 })
+sr.reveal('.contact__input', { delay: 400 })
+sr.reveal('.contact__button', { delay: 600 })
+
+/*==================== DARK LIGHT THEME ====================*/
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+if (themeButton) {
+    themeButton.addEventListener('click', () => {
+        // Add or remove the dark / icon theme
+        document.body.classList.toggle(darkTheme)
+        themeButton.classList.toggle(iconTheme)
+        // We save the theme and the current icon that the user chose
+        localStorage.setItem('selected-theme', getCurrentTheme())
+        localStorage.setItem('selected-icon', getCurrentIcon())
+    })
+}
